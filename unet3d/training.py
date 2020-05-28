fb20416c8bcf7e05a5cf7f82bd07a4d4c797ce82
maikia <maja_ka@hotmail.com>
2020-05-28 14:53:16 +0200

    correcting training to newer keras version

--- unet3d/training.py
+++ unet3d/training.py
@@ -12,7 +12,8 @@ from keras.models import load_model
 from unet3d.metrics import (dice_coefficient, dice_coefficient_loss, dice_coef, dice_coef_loss,
                             weighted_dice_coefficient_loss, weighted_dice_coefficient)
 
-K.set_image_dim_ordering('th')
+# K.set_image_dim_ordering('th')
+K.set_image_data_format('channels_first')
 
 # learning rate schedule
 def step_decay(epoch, initial_lrate, drop, epochs_drop):
