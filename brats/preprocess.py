51deed0510ed5856f6417ab4030438dc1eab7958
maikia <maja_ka@hotmail.com>
2020-06-05 13:52:51 +0200

    flake8 and cleaning up

--- brats/preprocess.py
+++ brats/preprocess.py
@@ -11,8 +11,6 @@ import SimpleITK as sitk
 import numpy as np
 from nipype.interfaces.ants import N4BiasFieldCorrection
 
-from brats.train import config
-
 
 def correct_bias(in_file, out_file, image_type=sitk.sitkFloat64):
     """
@@ -30,13 +28,13 @@ def correct_bias(in_file, out_file, image_type=sitk.sitkFloat64):
         return done.outputs.output_image
     except IOError:
         warnings.warn(RuntimeWarning("ANTs N4BIasFieldCorrection could not be "
-                                      "found. Will try using SimpleITK for "
-                                      "bias field correction which will take "
-                                      "much longer. To fix this problem, add "
-                                      "N4BiasFieldCorrection to your PATH "
-                                      "system variable. (example: "
-                                      "EXPORT PATH=${PATH}:/path/to/ants/bin)"
-                                    ))
+                                     "found. Will try using SimpleITK for "
+                                     "bias field correction which will take "
+                                     "much longer. To fix this problem, add "
+                                     "N4BiasFieldCorrection to your PATH "
+                                     "system variable. (example: "
+                                     "EXPORT PATH=${PATH}:/path/to/ants/bin)"
+                                     ))
         input_image = sitk.ReadImage(in_file, image_type)
         output_image = sitk.N4BiasFieldCorrection(input_image, input_image > 0)
         sitk.WriteImage(output_image, out_file)
@@ -47,23 +45,6 @@ def rescale(in_file, out_file, minimum=0, maximum=20000):
     image = sitk.ReadImage(in_file)
     sitk.WriteImage(sitk.RescaleIntensity(image, minimum, maximum), out_file)
     return os.path.abspath(out_file)
-
-
-def get_image(subject_folder, name):
-    file_card = os.path.join(subject_folder, "*" + name + ".nii.gz")
-    try:
-        return glob.glob(file_card)[0]
-    except IndexError:
-        raise RuntimeError("Could not find file matching {}".format(file_card))
-
-
-def check_origin(in_file, in_file2):
-    """ TODO: add doc"""
-    image = sitk.ReadImage(in_file)
-    image2 = sitk.ReadImage(in_file2)
-    if not image.GetOrigin() == image2.GetOrigin():
-        image.SetOrigin(image2.GetOrigin())
-        sitk.WriteImage(image, in_file)
 
 
 def normalize_image(in_file, out_file, bias_correction=True):
@@ -140,7 +121,6 @@ def convert_data_new(stroke_folder='../data/BIDS_lesions_zip/',
     subj_dirs = glob.glob(os.path.join(stroke_folder, "*"))
     subj_dirs = [subj_dir for subj_dir in subj_dirs if
                  os.path.isdir(subj_dir)]
-    site = stroke_folder.split('/')[-1]
     subjects = 0
 
     print('found {} subject directories'.format(len(subj_dirs)))
@@ -159,7 +139,7 @@ def convert_data_new(stroke_folder='../data/BIDS_lesions_zip/',
 
 
 def convert_data_old(stroke_folder='../data/ATLAS_R1.1/',
-                        out_folder='data/preprocessed'):
+                     out_folder='data/preprocessed'):
     """
     Finds the subjects data and writes it to a given output folder in a
     consistent format.
@@ -204,57 +184,62 @@ def convert_data_old(stroke_folder='../data/ATLAS_R1.1/',
 
 
 def convert_healthy_data(healthy_folder='../data/healthy',
-    out_folder='data/preprocessed/healthy'):
+                         out_folder='data/preprocessed/healthy'):
     """
     TODO: correct the doc
-    Preprocesses the BRATS data and writes it to a given output folder. Assumes the original folder structure.
+    Preprocesses the BRATS data and writes it to a given output folder. Assumes
+     the original folder structure.
     :param brats_folder: folder containing the original brats data
-    :param out_folder: output folder to which the preprocessed data will be written
+    :param out_folder: output folder to which the preprocessed data will be
+    written
     :param overwrite: set to True in order to redo all the preprocessing
-    :param no_bias_correction_modalities: performing bias correction could reduce the signal of certain modalities. If
-    concerned about a reduction in signal for a specific modality, specify by including the given modality in a list
+    :param no_bias_correction_modalities: performing bias correction could
+    reduce the signal of certain modalities. If
+    concerned about a reduction in signal for a specific modality, specify by
+    including the given modality in a list
     or tuple.
     :return:
     """
     """
     Copies and renames the data files to run within this program settings
-    for the healthy set of mri images. For each scan it adds the mask of the given
+    for the healthy set of mri images. For each scan it adds the mask of the
+    given
     size filled with 0s to signify no lesion
-    It is assumed that the healthy patient files are named 
+    It is assumed that the healthy patient files are named
     'sub_>number_t1_final<.nii.gz'
     """
     print('preparing: ', glob.glob(os.path.join(healthy_folder, "*")))
     # copy the file + filename
     # create out folder if does not already exist
-    if not os.path.exists(out_folder): 
-                        os.makedirs(out_folder)
+    if not os.path.exists(out_folder):
+        os.makedirs(out_folder)
 
     # create empty mask, same for all healthy patients
-    empty_mask = make_empty_mask(dim=[189,233,197]) # TODO: change to variable
-
+    # TODO: change dim to variable:
+    empty_mask = make_empty_mask(dim=[189, 233, 197])
     for subject_file in glob.glob(os.path.join(healthy_folder, "*")):
         subject = os.path.basename(subject_file)
-        new_subject_dir = os.path.join(out_folder, subject.split('_',2)[1])
-        if not os.path.exists(new_subject_dir): 
-                        os.makedirs(new_subject_dir)
-        out_file_t1 = os.path.abspath(os.path.join(new_subject_dir, 
-                        "t1.nii.gz"))
+        new_subject_dir = os.path.join(out_folder, subject.split('_', 2)[1])
+        if not os.path.exists(new_subject_dir):
+            os.makedirs(new_subject_dir)
+        out_file_t1 = os.path.abspath(os.path.join(new_subject_dir,
+                                                   "t1.nii.gz"))
 
         shutil.copy(subject_file, out_file_t1)
 
         # paste the mask with no lesion here
-        out_file_mask = os.path.abspath(os.path.join(new_subject_dir, 
-                        "truth.nii.gz"))
+        out_file_mask = os.path.abspath(os.path.join(new_subject_dir,
+                                                     "truth.nii.gz"))
         sitk.WriteImage(empty_mask, out_file_mask)
 
 
-def make_empty_mask(dim=[189,233,197]):
-    array_mask = np.zeros([dim[0],dim[1],dim[2]])
+def make_empty_mask(dim=[189, 233, 197]):
+    array_mask = np.zeros([dim[0], dim[1], dim[2]])
     empty_mask = sitk.GetImageFromArray(array_mask, isVector=False)
     return empty_mask
 
 
-def get_files(directory, name='t1', ext='.nii.gz'): #_image(subject_folder, subfolder='t01', name='t1'):
+def get_files(directory, name='t1', ext='.nii.gz'):
     # In the directory, it finds all the files including the given name and
     # with the givent extension ext
 
@@ -266,15 +251,15 @@ def get_files(directory, name='t1', ext='.nii.gz'): #_image(subject_folder, subf
 
 
 if __name__ == "__main__":
-    # TODO: add new data, '../BIDS_lesions_zip', format it correctly, check if
-    # usable
     # TODO: correct saving healthy data
     # TODO: look at all the data, all the scans if they look alright
+    # TODO: for now no normalization, rescale is done and the bias is not
+    # corrected, do we want it here?
     data_dir = '../../data/ATLAS_R1.1/'
     data_dir_new = '../../data/BIDS_lesions_zip/'
     healthy_data_dir = '../../data/healthy'
 
     output_data_dir = 'data/preprocessed/'
 
-    # convert_data_old(stroke_folder=data_dir, out_folder=output_data_dir)
+    convert_data_old(stroke_folder=data_dir, out_folder=output_data_dir)
     convert_data_new(stroke_folder=data_dir_new, out_folder=output_data_dir)
