6f13376897aabedf075028320f62055a56c44d79
maikia <maja_ka@hotmail.com>
2020-08-27 14:39:59 +0200

    clean up train.py

--- brats/train.py
+++ brats/train.py
@@ -1,62 +1,86 @@
 import glob
 import os
 
-from unet3d.data import write_data_to_file, open_data_file
+from unet3d.data import open_data_file, write_data_to_file
 from unet3d.generator import get_training_and_validation_generators
 from unet3d.model import isensee2017_model
 from unet3d.training import load_old_model, train_model
 
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 
 config = dict()
-config["image_shape"] = (144,144,144) #(128, 128, 128)  # This determines what shape the images will be cropped/resampled to.
+config["image_shape"] = (128, 128, 128)  # determines what shape the images
+# will be cropped/resampled to
 config["patch_shape"] = None  # switch to None to train on the whole image
-config["labels"] = (1)  # the label numbers on the input image
+config["labels"] = (1)  # the label numbers on the input image, eg (1, 2, 4)
 config["n_base_filters"] = 16
-config["n_labels"] = 1, #len(config["labels"])
-config["all_modalities"] = ["t1"] #, "t1ce", "flair", "t2"]
-config["training_modalities"] = config["all_modalities"]  # change this if you want to only use some of the modalities
+config["n_labels"] = 1,  # len(config["labels"])
+config["all_modalities"] = ["t1"]  # sets of data, eg: ["t1ce", "flair", "t2"]
+config["training_modalities"] = config["all_modalities"]  # change this if you
+# want to only use some of the modalities:
 config["nb_channels"] = len(config["training_modalities"])
 if "patch_shape" in config and config["patch_shape"] is not None:
-    config["input_shape"] = tuple([config["nb_channels"]] + list(config["patch_shape"]))
+    config["input_shape"] = tuple([config["nb_channels"]] +
+                                  list(config["patch_shape"])
+                                  )
 else:
-    config["input_shape"] = tuple([config["nb_channels"]] + list(config["image_shape"]))
+    config["input_shape"] = tuple([config["nb_channels"]] +
+                                  list(config["image_shape"])
+                                  )
 config["truth_channel"] = config["nb_channels"]
-config["deconvolution"] = True  # if False, will use upsampling instead of deconvolution
-
+config["deconvolution"] = True  # if False, will use upsampling instead
+# of deconvolution
 config["batch_size"] = 1
-config["validation_batch_size"] = 12
+config["validation_batch_size"] = 2
 config["n_epochs"] = 500  # cutoff the training after this many epochs
-config["patience"] = 10  # learning rate will be reduced after this many epochs if the validation loss is not improving
-config["early_stop"] = 50  # training will be stopped after this many epochs without the validation loss improving
+config["patience"] = 10  # learning rate will be reduced after this many epochs
+# if the validation loss is not improving
+config["early_stop"] = 50  # training will be stopped after this many epochs
+# without the validation loss improving
 config["initial_learning_rate"] = 5e-4
-config["learning_rate_drop"] = 0.5  # factor by which the learning rate will be reduced
-config["validation_split"] = 0.8  # portion of the data that will be used for training
-config["flip"] = False  # augments the data by randomly flipping an axis during
-config["permute"] = False  # data shape must be a cube. Augments the data by permuting in various directions
+config["learning_rate_drop"] = 0.5  # factor by which the learning rate
+# will be reduced
+config["validation_split"] = 0.8  # portion of the data that will be used for
+# training
+config["flip"] = False  # augments the data by randomly flipping
+# an axis
+config["permute"] = False  # data shape must be a cube. Augments the data by
+# permuting in various directions
 config["distort"] = None  # switch to None if you want no distortion
-config["augment"] = False #config["flip"] or config["distort"]
-config["validation_patch_overlap"] = 0  # if > 0, during training, validation patches will be overlapping
-config["training_patch_start_offset"] = (16, 16, 16)  # randomly offset the first patch index by up to this offset
-config["skip_blank"] = True  # if True, then patches without any target will be skipped
+config["augment"] = False
+config["validation_patch_overlap"] = 0  # if > 0, during training, validation
+# patches will be overlapping
+config["training_patch_start_offset"] = (16, 16, 16)  # randomly offset the
+# first patch index by up to this offset
+config["skip_blank"] = False  # if True, then patches without any target will
+# be skipped
 
 config["data_file"] = os.path.abspath("brats_data.h5")
-config["model_file"] = os.path.abspath("tumor_segmentation_model.h5")#os.path.abspath("isensee_2017_model.h5")
-config["training_file"] = os.path.abspath("training_ids.pkl") #("isensee_training_ids.pkl")
-config["validation_file"] = os.path.abspath("validation_ids.pkl") #("isensee_validation_ids.pkl")
-config["overwrite"] = False  # If True, will previous files. If False, will use previously written files.
+config["model_file"] = os.path.abspath("unet_model.h5")
+config["training_file"] = os.path.abspath("training_ids.pkl")
+config["validation_file"] = os.path.abspath("validation_ids.pkl")
+config["overwrite"] = False  # If True, will overwrite previous files.
+# If False, will use previously written files.
 
 
 def fetch_training_data_files(return_subject_ids=False):
     training_data_files = list()
     subject_ids = list()
-    for subject_dir in glob.glob(os.path.join(os.path.dirname(__file__), "data", "preprocessed", "*", "*")):
+    for subject_dir in glob.glob(
+            os.path.join(os.path.dirname(__file__),
+                         "data",
+                         "preprocessed",
+                         "*",
+                         "*"
+                         )):
         subject_ids.append(os.path.basename(subject_dir))
         subject_files = list()
         for modality in config["training_modalities"] + ["truth"]:
-            subject_files.append(os.path.join(subject_dir, modality + ".nii.gz"))
+            subject_files.append(os.path.join(subject_dir,
+                                              modality + ".nii.gz")
+                                 )
         training_data_files.append(tuple(subject_files))
     if return_subject_ids:
         return training_data_files, subject_ids
@@ -67,9 +91,12 @@ def fetch_training_data_files(return_subject_ids=False):
 def main(overwrite=False):
     # convert input images into an hdf5 file
     if overwrite or not os.path.exists(config["data_file"]):
-        training_files, subject_ids = fetch_training_data_files(return_subject_ids=True)
+        training_files, subject_ids = fetch_training_data_files(
+            return_subject_ids=True)
 
-        write_data_to_file(training_files, config["data_file"], image_shape=config["image_shape"],
+        write_data_to_file(training_files,
+                           config["data_file"],
+                           image_shape=config["image_shape"],
                            subject_ids=subject_ids)
     data_file_opened = open_data_file(config["data_file"])
 
@@ -77,12 +104,15 @@ def main(overwrite=False):
         model = load_old_model(config["model_file"])
     else:
         # instantiate new model
-        model = isensee2017_model(input_shape=config["input_shape"], n_labels=config["n_labels"],
-                                  initial_learning_rate=config["initial_learning_rate"],
-                                  n_base_filters=config["n_base_filters"])
+        model = isensee2017_model(
+            input_shape=config["input_shape"],
+            n_labels=config["n_labels"],
+            initial_learning_rate=config["initial_learning_rate"],
+            n_base_filters=config["n_base_filters"])
 
     # get training and testing generators
-    train_generator, validation_generator, n_train_steps, n_validation_steps = get_training_and_validation_generators(
+    (train_generator, validation_generator, n_train_steps,
+     n_validation_steps) = get_training_and_validation_generators(
         data_file_opened,
         batch_size=config["batch_size"],
         data_split=config["validation_split"],
