be7e495173296a776edf759aeb0e5c2b59ac19eb
maikia <maja_ka@hotmail.com>
2020-08-28 16:43:30 +0200

    flake8 on prediction.py

--- unet3d/prediction.py
+++ unet3d/prediction.py
@@ -4,10 +4,12 @@ import nibabel as nib
 import numpy as np
 import tables
 
+from .augment import generate_permutation_keys, permute_data
+from .augment import reverse_permute_data
 from .training import load_old_model
 from .utils import pickle_load
-from .utils.patches import reconstruct_from_patches, get_patch_from_3d_data, compute_patch_indices
-from .augment import permute_data, generate_permutation_keys, reverse_permute_data
+from .utils.patches import compute_patch_indices, get_patch_from_3d_data
+from .utils.patches import reconstruct_from_patches
 
 
 def patch_wise_prediction(model, data, overlap=0, batch_size=1, permute=False):
@@ -20,12 +22,15 @@ def patch_wise_prediction(model, data, overlap=0, batch_size=1, permute=False):
     """
     patch_shape = tuple([int(dim) for dim in model.input.shape[-3:]])
     predictions = list()
-    indices = compute_patch_indices(data.shape[-3:], patch_size=patch_shape, overlap=overlap)
+    indices = compute_patch_indices(data.shape[-3:], patch_size=patch_shape,
+                                    overlap=overlap)
     batch = list()
     i = 0
     while i < len(indices):
         while len(batch) < batch_size:
-            patch = get_patch_from_3d_data(data[0], patch_shape=patch_shape, patch_index=indices[i])
+            patch = get_patch_from_3d_data(data[0],
+                                           patch_shape=patch_shape,
+                                           patch_index=indices[i])
             batch.append(patch)
             i += 1
         prediction = predict(model, np.asarray(batch), permute=permute)
@@ -33,7 +38,8 @@ def patch_wise_prediction(model, data, overlap=0, batch_size=1, permute=False):
         for predicted_patch in prediction:
             predictions.append(predicted_patch)
     output_shape = [int(model.output.shape[1])] + list(data.shape[-3:])
-    return reconstruct_from_patches(predictions, patch_indices=indices, data_shape=output_shape)
+    return reconstruct_from_patches(predictions, patch_indices=indices,
+                                    data_shape=output_shape)
 
 
 def get_prediction_labels(prediction, threshold=0.5, labels=None):
@@ -62,33 +68,39 @@ def predict_and_get_image(model, data, affine):
 
 
 def predict_from_data_file_and_get_image(model, open_data_file, index):
-    return predict_and_get_image(model, open_data_file.root.data[index], open_data_file.root.affine)
+    return predict_and_get_image(model, open_data_file.root.data[index],
+                                 open_data_file.root.affine)
 
 
-def predict_from_data_file_and_write_image(model, open_data_file, index, out_file):
+def predict_from_data_file_and_write_image(model, open_data_file, index,
+                                           out_file):
     image = predict_from_data_file_and_get_image(model, open_data_file, index)
     image.to_filename(out_file)
 
 
-def prediction_to_image(prediction, affine, label_map=False, threshold=0.5, labels=None):
+def prediction_to_image(prediction, affine, label_map=False, threshold=0.5,
+                        labels=None):
     if prediction.shape[1] == 1:
         data = prediction[0, 0]
         if label_map:
             label_map_data = np.zeros(prediction[0, 0].shape, np.int8)
             if labels:
-                label = labels #[0]
+                label = labels  # [0]
             else:
                 label = 1
             label_map_data[data > threshold] = label
             data = label_map_data
     elif prediction.shape[1] > 1:
         if label_map:
-            label_map_data = get_prediction_labels(prediction, threshold=threshold, labels=labels)
+            label_map_data = get_prediction_labels(prediction,
+                                                   threshold=threshold,
+                                                   labels=labels)
             data = label_map_data[0]
         else:
             return multi_class_prediction(prediction, affine)
     else:
-        raise RuntimeError("Invalid prediction array shape: {0}".format(prediction.shape))
+        raise RuntimeError("Invalid prediction array shape: {0}".format(
+            prediction.shape))
     return nib.Nifti1Image(data, affine)
 
 
@@ -99,16 +111,20 @@ def multi_class_prediction(prediction, affine):
     return prediction_images
 
 
-def run_validation_case(data_index, output_dir, model, data_file, training_modalities,
-                        output_label_map=False, threshold=0.5, labels=None, overlap=16, permute=False):
+def run_validation_case(data_index, output_dir, model, data_file,
+                        training_modalities, output_label_map=False,
+                        threshold=0.5, labels=None, overlap=16, permute=False):
     """
     Runs a test case and writes predicted images to file.
-    :param data_index: Index from of the list of test cases to get an image prediction from.
+    :param data_index: Index from of the list of test cases to get an image
+        prediction from.
     :param output_dir: Where to write prediction images.
-    :param output_label_map: If True, will write out a single image with one or more labels. Otherwise outputs
+    :param output_label_map: If True, will write out a single image with one
+        or more labels. Otherwise outputs
     the (sigmoid) prediction values from the model.
-    :param threshold: If output_label_map is set to True, this threshold defines the value above which is 
-    considered a positive result and will be assigned a label.  
+    :param threshold: If output_label_map is set to True, this threshold
+        defines the value above which is considered a positive result and will
+        be assigned a label.
     :param labels:
     :param training_modalities:
     :param data_file:
@@ -121,7 +137,8 @@ def run_validation_case(data_index, output_dir, model, data_file, training_modal
     test_data = np.asarray([data_file.root.data[data_index]])
     for i, modality in enumerate(training_modalities):
         image = nib.Nifti1Image(test_data[0, i], affine)
-        image.to_filename(os.path.join(output_dir, "data_{0}.nii.gz".format(modality)))
+        image.to_filename(os.path.join(output_dir, "data_{0}.nii.gz".format(
+            modality)))
 
     test_truth = nib.Nifti1Image(data_file.root.truth[data_index][0], affine)
     test_truth.to_filename(os.path.join(output_dir, "truth.nii.gz"))
@@ -130,29 +147,42 @@ def run_validation_case(data_index, output_dir, model, data_file, training_modal
     if patch_shape == test_data.shape[-3:]:
         prediction = predict(model, test_data, permute=permute)
     else:
-        prediction = patch_wise_prediction(model=model, data=test_data, overlap=overlap, permute=permute)[np.newaxis]
-    prediction_image = prediction_to_image(prediction, affine, label_map=output_label_map, threshold=threshold,
+        prediction = patch_wise_prediction(model=model, data=test_data,
+                                           overlap=overlap,
+                                           permute=permute)[np.newaxis]
+    prediction_image = prediction_to_image(prediction, affine,
+                                           label_map=output_label_map,
+                                           threshold=threshold,
                                            labels=labels)
     if isinstance(prediction_image, list):
         for i, image in enumerate(prediction_image):
-            image.to_filename(os.path.join(output_dir, "prediction_{0}.nii.gz".format(i + 1)))
+            image.to_filename(os.path.join(
+                output_dir, "prediction_{0}.nii.gz".format(i + 1)))
     else:
-        prediction_image.to_filename(os.path.join(output_dir, "prediction.nii.gz"))
+        prediction_image.to_filename(os.path.join(output_dir,
+                                                  "prediction.nii.gz"))
 
 
-def run_validation_cases(validation_keys_file, model_file, training_modalities, labels, hdf5_file,
-                         output_label_map=False, output_dir=".", threshold=0.5, overlap=16, permute=False):
+def run_validation_cases(validation_keys_file, model_file, training_modalities,
+                         labels, hdf5_file,
+                         output_label_map=False, output_dir=".", threshold=0.5,
+                         overlap=16, permute=False):
     validation_indices = pickle_load(validation_keys_file)
     model = load_old_model(model_file)
     data_file = tables.open_file(hdf5_file, "r")
     for index in validation_indices:
         if 'subject_ids' in data_file.root:
-            case_directory = os.path.join(output_dir, data_file.root.subject_ids[index].decode('utf-8'))
+            case_directory = os.path.join(
+                output_dir, data_file.root.subject_ids[index].decode('utf-8'))
         else:
-            case_directory = os.path.join(output_dir, "validation_case_{}".format(index))
-        run_validation_case(data_index=index, output_dir=case_directory, model=model, data_file=data_file,
-                            training_modalities=training_modalities, output_label_map=output_label_map, labels=labels,
-                            threshold=threshold, overlap=overlap, permute=permute)
+            case_directory = os.path.join(
+                output_dir, "validation_case_{}".format(index))
+        run_validation_case(data_index=index, output_dir=case_directory,
+                            model=model, data_file=data_file,
+                            training_modalities=training_modalities,
+                            output_label_map=output_label_map, labels=labels,
+                            threshold=threshold, overlap=overlap,
+                            permute=permute)
     data_file.close()
 
 
@@ -160,7 +190,8 @@ def predict(model, data, permute=False):
     if permute:
         predictions = list()
         for batch_index in range(data.shape[0]):
-            predictions.append(predict_with_permutations(model, data[batch_index]))
+            predictions.append(predict_with_permutations(model,
+                                                         data[batch_index]))
         return np.asarray(predictions)
     else:
         return model.predict(data)
@@ -170,5 +201,6 @@ def predict_with_permutations(model, data):
     predictions = list()
     for permutation_key in generate_permutation_keys():
         temp_data = permute_data(data, permutation_key)[np.newaxis]
-        predictions.append(reverse_permute_data(model.predict(temp_data)[0], permutation_key))
+        predictions.append(reverse_permute_data(model.predict(temp_data)[0],
+                                                permutation_key))
     return np.mean(predictions, axis=0)
